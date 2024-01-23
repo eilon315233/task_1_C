8f4dd13d926ff8f8c9c87b04e1c36f1f1b853774
eilon315233 <156177204+eilon315233@users.noreply.github.com>
2024-01-23 22:36:47 +0200

    Update advancedClassificationRecursion.c

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -1,33 +1,15 @@
 #include <stdio.h>
 
-/* will return if a number is Armstrong number
-An Armstrong number is an n-digit number that is equal to the sum of the nth powers of its digits.
-For Example: 407 = 43 + 03 + 73 = 64 + 0 + 343 = 407
-*/
-
-int isArmstrong(int a) {
-    int numDigits = countDigits(a);
-    return (isArmstrongRecursive(a, a, numDigits) == a);
-}
-
-/* will return if a number is a palindrome */
-int isPalindrome(int a)
-{
-  
-}
-
-// my function
-
-int pow(int numberD,int digit)
-{
-    int i,sum = 1;
-    for(i=0;i<numberD;i++)
-    {
-        sum = sum*digit;
+// Function to calculate power
+int power(int numberD, int digit) {
+    int i, sum = 1;
+    for (i = 0; i < numberD; i++) {
+        sum = sum * digit;
     }
     return sum;
 }
 
+// Function to count digits using recursion
 int countDigits(int a) {
     if (a == 0) {
         return 0;
@@ -36,12 +18,41 @@ int countDigits(int a) {
     }
 }
 
+// Recursive function to check if a number is an Armstrong number
 int isArmstrongRecursive(int a, int originalA, int numDigits) {
     if (a == 0) {
         return (originalA == 0);
     } else {
         int digit = a % 10;
-        int poweredDigit = pow(digit, numDigits);
+        int poweredDigit = power(numDigits, digit); // Fix: Corrected the order of arguments
         return poweredDigit + isArmstrongRecursive(a / 10, originalA - poweredDigit, numDigits);
     }
 }
+
+// Recursive function to check if a number is a palindrome
+int isPalindromeHelper(int num, int original) { // Fix: Added 'original' as an argument
+    int reverse = 0;
+
+    while (num > 0) {
+        int digit = num % 10;
+        reverse = reverse * 10 + digit;
+        num = num / 10;
+    }
+
+    return (original == reverse);
+}
+
+// Function to check if a number is an Armstrong number
+int isArmstrong(int a) {
+    int numDigits = countDigits(a);
+    return (isArmstrongRecursive(a, a, numDigits) == a);
+}
+
+// Function to check if a number is a palindrome
+int isPalindrome(int a) {
+    if (a < 0)
+        return 0;  // Negative numbers are not palindromes
+
+    return isPalindromeHelper(a, a); // Fix: Added 'a' as an argument
+}
+
